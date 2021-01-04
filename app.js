afd7da6890f0fe475831f24bf2db889a1e18d0f1
Jurgen Arias <jurgen.as@gmail.com>
2021-01-04 15:26:45 -0500

    Director

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ d3.csv("movies.csv").then(function (data) {
       // console.log(output[i]['avg_vote'])
       // d3.select("tbody>tr>td").text(output[i]['original_title']);
       d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>"+"<a href=https://www.imdb.com/title/"+output[i]['imdb_title_id']+" target='_blank'>"+(output[i]['original_title'])+"</a>"
-      + "</td>" +"<td>" +(output[i]['avg_vote'])+"</td>" +"<td>" +(output[i]['year'])+"</td>" +"<td>" +(output[i]['description'])+"</td") }   
+      + "</td>" +"<td>" +(output[i]['avg_vote'])+"</td>" +"<td>" +(output[i]['year'])+"</td>"  +"<td>" +(output[i]['director'])+"</td>"+"<td>" +(output[i]['description'])+"</td>") }
   };
   window.resizeTo(screen.width,screen.height)
 
